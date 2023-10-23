41229f6ee76864d53c8681a9c953a2c01c63786b
burczan <natalia.wajnor@gmail.com>
2023-10-23 14:32:25 +0200

    Updates

--- src/lib/components/Dt.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  export let title: string;
-</script>
-
-<dt>
-  <span class="nes-text is-primary">{title}</span>
-</dt>
--- src/lib/components/SvelteHead.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  export let title: string;
-</script>
-
-<svelte:head>
-  <title>Me | {title}</title>
-  <meta name="description" content={title} />
-</svelte:head>
