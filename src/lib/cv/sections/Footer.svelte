f54d8bde0862666cdc1a8bf94e18d325e2fc7702
burczan <natalia.wajnor@gmail.com>
2023-10-31 08:55:12 +0100

    fix app building

--- src/lib/cv/sections/Footer.svelte
+++ src/lib/cv/sections/Footer.svelte
@@ -1,7 +1,11 @@
+<script lang="ts">
+  import { base } from "$app/paths";
+</script>
+
 <p>
   Handmade with <a href="https://svelte.dev/">Svelte</a>,
   <a href="https://milligram.io/">Milligram</a>,
   <a href="https://fontawesome.com/">Font Awesome</a>, and
   <a href="https://github.com/cognitom/paper-css">Paper CSS</a> by
-  <a href="#contact">me</a>.
+  <a href="{base}/cv#contact">me</a>.
 </p>
