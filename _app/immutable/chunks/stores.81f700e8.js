28136b9ab0c7551989f7a1caee5be9203ff97fb9
burczan <natalia.wajnor@gmail.com>
2024-08-29 08:35:55 +0200

    update gh-pages

---
+++ _app/immutable/chunks/stores.81f700e8.js
@@ -0,0 +1,1 @@
+import{s as e}from"./singletons.3e73d1b0.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
