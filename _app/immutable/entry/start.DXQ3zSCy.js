c5e1136efb2e15cf1e2a6194a3ef706301365db6
burczan <natalia.wajnor@gmail.com>
2024-09-03 15:33:17 +0200

    update gh-pages

--- _app/immutable/entry/start.DXQ3zSCy.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.duBkmUfP.js";export{t as start};
---
+++ _app/immutable/nodes/4.D4DKP18K.js
@@ -0,0 +1,1 @@
+import{a as o}from"../chunks/index.COh0j1S2.js";import{component as l}from"./0.BoJvoXlh.js";const e=async()=>({about:o}),r=Object.freeze(Object.defineProperty({__proto__:null,load:e},Symbol.toStringTag,{value:"Module"}));export{l as component,r as universal};
